6d1c40515b7affe7b652885cd084a1f1780279b6
Zegzus <kacpermalach04@gmail.com>
2026-08-02 16:33:05 +0200

    Add and run Ansible playbook for Docker and UFW setup

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "learnit-ssh-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-  key_name             = var.key_name
+  key_name             = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "LearnIT-DevOps-Server"
